2c4705220dbf3d9b9ab5762de83f47131054f3cf
Daniel Lopez <dlopez807@gmail.com>
2016-05-22 15:35:21 -0700

    moved code outside src

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var del = require('del');
+var runSequence = require('run-sequence');
+
+gulp.task('hello'), function() {
+	console.log('hello there');
+};
+
+gulp.task('sass', function() {
+	return gulp.src('src/scss/**/*.scss')
+		.pipe(sass()) // converts sass to css with gulp-sass
+		.pipe(gulp.dest('src/css'))
+		.pipe(browserSync.reload({
+			stream: true
+		}))
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function() {
+	gulp.watch('src/scss/**/*.scss', ['sass']);
+	gulp.watch('src/*.html', browserSync.reload);
+	gulp.watch('src/js/**/*.js', browserSync.reload);
+
+});
+
+gulp.task('browserSync', function() {
+	browserSync.init({
+		server: {
+			baseDir: 'src'
+		},
+		port: 8791,
+		browser: "google chrome"
+	})
+});
+
+gulp.task('useref', function() {
+	return gulp.src('src/*.html')
+		.pipe(useref())
+		// minifies only if it's a javascript file
+		.pipe(gulpIf('*.js', uglify()))
+		// minifies only if it's a css file
+		.pipe(gulpIf('*.css', cssnano()))
+		.pipe(gulp.dest('dist'))
+});
+
+gulp.task('clean:dist', function() {
+	return del.sync('dist');
+});
+
+gulp.task('build', function(callback) {
+	runSequence('clean:dist', ['sass', 'useref'], callback);
+});
+
+gulp.task('default', function(callback) {
+	runSequence(['sass', 'browserSync', 'watch'], callback);
+})
